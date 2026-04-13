e069027bbfc19de821413b9f788012e1cbf9c48b
EkssTV <matthieu.decreme@gmail.com>
2026-04-13 09:40:40 +0200

    feat(Controllers): Bourlders Controllers + routes

---
+++ server/src/controllers/tbBouldersControllers.ts
@@ -0,0 +1,156 @@
+import type { Request, Response } from "express";
+import { tbUsers,tbDifficulties,tbBoulders,tbAreaGyms,tbGyms} from "../models";
+import { postElement,delElement } from "../utils/simpleControllers";
+
+export const getAllBoulders = async (req : Request,res : Response ) =>{
+    try {
+        const json = await tbBoulders.findAll({
+            include: 
+            [{model: tbDifficulties,
+            as: "difficulty",
+            attributes: ["difficultyId","difficultyColorName","difficultyFrenchScale","difficultyVerminScale"]},
+            {
+            model: tbUsers,
+            as: "setter",
+            attributes: ["userId","userFName","userLName","userPseudo"]
+            },{
+            model: tbAreaGyms,
+            as: "area",
+            attributes: ["areaId","areaName","areaDesc"],
+            include :[ {
+                model: tbGyms,
+                as: "gym",
+                attributes: ["gymId","gymName"]
+            }]
+            }]
+        });
+        res.status(200).json(json);
+
+    } catch (error) {
+    res.status(500).json({ error: (error as any).message });
+};  
+};
+export const getBoulderbyPk = async (req:Request,res: Response) =>{
+    try {
+        const id = req.params.id as string;
+        const json = await tbUsers.findByPk(id,{
+            include:
+            [{model: tbDifficulties,
+            as: "difficulty",
+            attributes: ["difficultyId","difficultyColorName","difficultyFrenchScale","difficultyVerminScale"]},
+            {
+            model: tbUsers,
+            as: "setter",
+            attributes: ["userId","userFName","userLName","userPseudo"]
+            },{
+            model: tbAreaGyms,
+            as: "areaGyms",
+            attributes: ["areaId","areaName","areaDesc"],
+            include :[ {
+                model: tbGyms,
+                as: "gym",
+                attributes: ["gymId","gymName"]
+            }]
+            }]
+        })
+        res.status(200).json(json);
+    } catch (error) {
+        res.status(500).json({ error: (error as any).message })
+    }
+};
+export const getBoulderByArea = async (req:Request,res:Response) => {
+    try {
+        const id = req.params.id as string;
+        const json = await tbBoulders.findAll({
+            where : {areaId:id},
+            include: 
+            [{model: tbDifficulties,
+            as: "difficulty",
+            attributes: ["difficultyId","difficultyColorName","difficultyFrenchScale","difficultyVerminScale"]},
+            {
+            model: tbUsers,
+            as: "setter",
+            attributes: ["userId","userFName","userLName","userPseudo"]
+            },{
+            model: tbAreaGyms,
+            as: "area",
+            attributes: ["areaId","areaName","areaDesc"],
+            include :[ {
+                model: tbGyms,
+                as: "gym",
+                attributes: ["gymId","gymName"]
+            }]
+            }]
+        });
+        res.status(200).json(json);
+
+    } catch (error) {
+    res.status(500).json({ error: (error as any).message });
+};  
+};
+export const getBoulderBySetter = async (req:Request,res:Response) => {
+    try {
+        const id = req.params.id as string;
+        const json = await tbBoulders.findAll({
+            where : {userId:id},
+            include: 
+            [{model: tbDifficulties,
+            as: "difficulty",
+            attributes: ["difficultyId","difficultyColorName","difficultyFrenchScale","difficultyVerminScale"]},
+            {
+            model: tbUsers,
+            as: "setter",
+            attributes: ["userId","userFName","userLName","userPseudo"]
+            },{
+            model: tbAreaGyms,
+            as: "area",
+            attributes: ["areaId","areaName","areaDesc"],
+            include :[ {
+                model: tbGyms,
+                as: "gym",
+                attributes: ["gymId","gymName"]
+            }]
+            }]
+        });
+        res.status(200).json(json);
+
+    } catch (error) {
+    res.status(500).json({ error: (error as any).message });
+};  
+};
+export const getBoulderByDifficulty = async (req:Request,res:Response) => {
+    try {
+        const id = req.params.id as string;
+        const json = await tbBoulders.findAll({
+            where : {difficultyId:id},
+            include: 
+            [{model: tbDifficulties,
+            as: "difficulty",
+            attributes: ["difficultyId","difficultyColorName","difficultyFrenchScale","difficultyVerminScale"]},
+            {
+            model: tbUsers,
+            as: "setter",
+            attributes: ["userId","userFName","userLName","userPseudo"]
+            },{
+            model: tbAreaGyms,
+            as: "area",
+            attributes: ["areaId","areaName","areaDesc"],
+            include :[ {
+                model: tbGyms,
+                as: "gym",
+                attributes: ["gymId","gymName"]
+            }]
+            }]
+        });
+        res.status(200).json(json);
+
+    } catch (error) {
+    res.status(500).json({ error: (error as any).message });
+};  
+};
+export const postBoulder = async (req:Request,res:Response) => {
+    postElement(req,res,tbBoulders)
+};
+export const deleteBoulder = async (req:Request,res:Response)=>{
+    delElement(req,res,tbBoulders)
+};
--- server/src/controllers/tbUsersControllers.ts
+++ server/src/controllers/tbUsersControllers.ts
@@ -1,9 +1,24 @@
 import type { Request, Response } from "express";
-import { tbUsers } from "../models";
+import { tbUsers,tbDifficulties,tbStatus,tbProfilePictures } from "../models";
+import { postElement,delElement } from "../utils/simpleControllers";
 
 export const getAllUsers = async (req : Request,res : Response ) =>{
     try {
-        const usersAll = await tbUsers.findAll({include: ["difficulty", "profilePicture", "status"]});
+        const usersAll = await tbUsers.findAll({
+            include: 
+            [{model: tbDifficulties,
+            as: "difficulty",
+            attributes: ["difficultyId","difficultyColorName","difficultyFrenchScale","difficultyVerminScale"]},
+            {
+            model: tbProfilePictures,
+            as: "profilePicture",
+            attributes: ["pictureId","pictureLink","pictureLegend"]
+            },{
+            model: tbStatus,
+            as: "status",
+            attributes: ["statusId","statusName"]
+            }]
+        });
         res.status(200).json(usersAll);
 
     } catch (error) {
@@ -13,30 +28,27 @@ export const getAllUsers = async (req : Request,res : Response ) =>{
 export const getUserbyPk = async (req:Request,res: Response) =>{
     try {
         const id = req.params.id as string;
-        const user = await tbUsers.findByPk(id,{include:["difficulty", "profilePicture", "status"]});
+        const user = await tbUsers.findByPk(id,{
+            include: [{model: tbDifficulties,
+            as: "averageDifficultiy",
+            attributes: ["difficultyId","difficultyColorName","difficultyFrenchScale","difficultyVerminScale"]},
+            {
+            model: tbProfilePictures,
+            as: "profilePicture",
+            attributes: ["pictureId","pictureLink","pictureLegend"]
+            },{
+            model: tbStatus,
+            as: "status",
+            attributes: ["statusId","statusName"]
+            }]});
         res.status(200).json(user);
     } catch (error) {
         res.status(500).json({ error: (error as any).message })
     }
 };
 export const postUsers = async (req:Request,res:Response) => {
-    try {
-        const user = await tbUsers.create(req.body);
-        res.status(201).json(user);
-    } catch (error) {
-        res.status(500).json({ error: (error as any).message });
-    }
-    
+    postElement(req,res,tbUsers)
 };
 export const deleteUsers = async (req:Request,res:Response)=>{
-    try {
-            const id = req.params.id as string;
-            const user = await tbUsers.findByPk(id);
-            if (!user) return res.status(404).json({error : "pas de user ayant cet ID"});
-            await user.destroy();
-            res.status(204).json({message: `User ${id} a été supprimé`});
-    } catch (error) {
-        res.status(500).json({ error: (error as any).message });
-    };
-
+    delElement(req,res,tbUsers)
 };
