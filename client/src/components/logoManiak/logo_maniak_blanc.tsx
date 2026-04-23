6e7667ba54436334278fb892e7662a6b61a40d8a
Boureym <b.junion@students.ephec.be>
2026-04-23 14:26:19 +0200

    Resolution bug image

--- client/src/components/logoManiak/logo_maniak_blanc.tsx
+++ client/src/components/logoManiak/logo_maniak_blanc.tsx
@@ -1,5 +1,7 @@
+import logo_maniak_blanc from "../../assets/img/Logo_maniak_blanc.png"
+
 function WhiteManiakLogo(){
-    return <div id="zoneWhiteLogoManiakHome"><img id="whiteLogoManiakHome" src="./img/Logo_maniak_blanc.png" alt="Logo maniak blanc" loading="lazy"></img></div>
+    return <div id="zoneWhiteLogoManiakHome"><img id="whiteLogoManiakHome" src={logo_maniak_blanc} alt="Logo maniak blanc" loading="lazy"></img></div>
 }
 
 export default WhiteManiakLogo
