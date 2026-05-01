557c9bfdfee9665666cdc7eb8aaf81e5eda0b1af
EkssTV <matthieu.decreme@gmail.com>
2026-05-01 14:55:13 +0200

    update(Controllers) : Comments count and fix

--- server/src/controllers/tbCommentsControllers.ts
+++ server/src/controllers/tbCommentsControllers.ts
@@ -6,32 +6,66 @@ import { Op, Sequelize } from "sequelize"
 export const getAllComments = async (req : Request,res : Response ) =>{
     try {
         const json = await tbComments.findAll({
-            include: 
-            [  
-                {
-                model: tbUsers,
-                as: "author",
-                attributes: ["userId","userFName","userLName","userPseudo"]
-                },{
-                model: tbBoulders,
-                as:"boulder",
-                attributes: ["boulderId","boulderDesc","boulderLink","boulderReleaseDate","boulderEndDate","difficultyId","userId","areaId","boulderImageUrl"],
-                include :[ {
-                model: tbAreaGyms,
-                as: "area",
-                attributes: ["areaId","areaName"]
-                            }]
-                },{
-                model: tbReplies,
-                as: "isReplyOf",
-                required: false,
-                attributes: []
-                },
-            ],
-            where: Sequelize.where(
-                    Sequelize.col("isReplyOf.commentsrepliesId"),
-                    null
-                )
+            where: {
+        [Op.and]: [
+          Sequelize.where(
+            Sequelize.col("isReplyOf.commentsrepliesId"),
+            null
+          )
+        ]
+      },
+      attributes: {
+        include: [
+          [
+            Sequelize.fn("COUNT", Sequelize.col("replies.commentsId")),
+            "replyCount"
+          ]
+        ]
+      },
+      include: [
+        {
+          model: tbUsers,
+          as: "author",
+          attributes: ["userId", "userFName", "userLName", "userPseudo"]
+        },
+        {
+          model: tbBoulders,
+          as: "boulder",
+          attributes: [
+            "boulderId", "boulderDesc", "boulderLink",
+            "boulderReleaseDate", "boulderEndDate",
+            "difficultyId", "userId", "areaId", "boulderImageUrl"
+          ],
+          include: [
+            {
+              model: tbAreaGyms,
+              as: "area",
+              attributes: ["areaId", "areaName"]
+            }
+          ]
+        },
+        // Filtre root comments
+        {
+          model: tbReplies,
+          as: "isReplyOf",
+          required: false,
+          attributes: []
+        },
+        // Compte les replies
+        {
+          model: tbReplies,
+          as: "replies",
+          required: false,
+          attributes: []
+        }
+      ],
+      group: [
+        "tbComments.commentsId",
+        "author.userId",
+        "boulder.boulderId",
+        "boulder->area.areaId"
+      ],
+      order: [["commentsId", "DESC"]]
         });
         res.status(200).json(json);
 
@@ -42,29 +76,68 @@ export const getAllComments = async (req : Request,res : Response ) =>{
 export const getCommentsbyPk = async (req:Request,res: Response) =>{
     try {
         const id = req.params.id as string;
-        const json = await tbComments.findByPk(id,{
-            include:
-            [  
-                {
-                model: tbUsers,
-                as: "author",
-                attributes: ["userId","userFName","userLName","userPseudo"]
-                },{
-                model: tbBoulders,
-                as:"boulder",
-                attributes: ["boulderId","boulderDesc","boulderLink","boulderReleaseDate","boulderEndDate","difficultyId","userId","areaId","boulderImageUrl"],
-                include :[ {
-                model: tbAreaGyms,
-                as: "area",
-                attributes: ["areaId","areaName"]
-                            }]
-                },{
-                model: tbReplies,
-                as: "isReplyOf",
-                required: false,
-                attributes: []
-                },
-            ],
+        const json = await tbComments.findAll({
+            where: {
+        [Op.and]: [
+          { commentsId: id },
+          Sequelize.where(
+            Sequelize.col("isReplyOf.commentsrepliesId"),
+            null
+          )
+        ]
+      },
+      attributes: {
+        include: [
+          [
+            Sequelize.fn("COUNT", Sequelize.col("replies.commentsId")),
+            "replyCount"
+          ]
+        ]
+      },
+      include: [
+        {
+          model: tbUsers,
+          as: "author",
+          attributes: ["userId", "userFName", "userLName", "userPseudo"]
+        },
+        {
+          model: tbBoulders,
+          as: "boulder",
+          attributes: [
+            "boulderId", "boulderDesc", "boulderLink",
+            "boulderReleaseDate", "boulderEndDate",
+            "difficultyId", "userId", "areaId", "boulderImageUrl"
+          ],
+          include: [
+            {
+              model: tbAreaGyms,
+              as: "area",
+              attributes: ["areaId", "areaName"]
+            }
+          ]
+        },
+        // Filtre root comments
+        {
+          model: tbReplies,
+          as: "isReplyOf",
+          required: false,
+          attributes: []
+        },
+        // Compte les replies
+        {
+          model: tbReplies,
+          as: "replies",
+          required: false,
+          attributes: []
+        }
+      ],
+      group: [
+        "tbComments.commentsId",
+        "author.userId",
+        "boulder.boulderId",
+        "boulder->area.areaId"
+      ],
+      order: [["commentsId", "DESC"]]
         })
         res.status(200).json(json);
     } catch (error) {
@@ -75,74 +148,148 @@ export const getCommentsbyUser = async (req:Request,res: Response) =>{
     try {
         const id = req.params.id as string;
         const json = await tbComments.findAll({
-            where:{userId:id},
-            include:
-            [  
-                {
-                model: tbUsers,
-                as: "author",
-                attributes: ["userId","userFName","userLName","userPseudo"]
-                },{
-                model: tbBoulders,
-                as:"boulder",
-                attributes: ["boulderId","boulderDesc","boulderLink","boulderReleaseDate","boulderEndDate","difficultyId","userId","areaId","boulderImageUrl"],
-                include :[ {
-                model: tbAreaGyms,
-                as: "area",
-                attributes: ["areaId","areaName"]
-                            }]
-                },{
-                model: tbReplies,
-                as: "isReplyOf",
-                required: false,
-                attributes: []
-                },
-            ],
-        })
-        res.status(200).json(json);
-    } catch (error) {
-        res.status(500).json({ error: (error as any).message })
-    }
-};
-export const getCommentsbyBoulders = async (req:Request,res: Response) =>{
-    try {
-        const id = req.params.id as string;
-        const json = await tbComments.findAll({
-            where:{[Op.and]: [
+           where: {
+        [Op.and]: [
           { userId: id },
           Sequelize.where(
             Sequelize.col("isReplyOf.commentsrepliesId"),
             null
           )
-        ]},
-            include:
-            [  
-                {
-                model: tbUsers,
-                as: "author",
-                attributes: ["userId","userFName","userLName","userPseudo"]
-                },{
-                model: tbBoulders,
-                as:"boulder",
-                attributes: ["boulderId","boulderDesc","boulderLink","boulderReleaseDate","boulderEndDate","difficultyId","userId","areaId","boulderImageUrl"],
-                include :[ {
-                model: tbAreaGyms,
-                as: "area",
-                attributes: ["areaId","areaName"]
-                            }]
-                },{
-                model: tbReplies,
-                as: "isReplyOf",
-                required: false,
-                attributes: []
-                },
-            ],
+        ]
+      },
+      attributes: {
+        include: [
+          [
+            Sequelize.fn("COUNT", Sequelize.col("replies.commentsId")),
+            "replyCount"
+          ]
+        ]
+      },
+      include: [
+        {
+          model: tbUsers,
+          as: "author",
+          attributes: ["userId", "userFName", "userLName", "userPseudo"]
+        },
+        {
+          model: tbBoulders,
+          as: "boulder",
+          attributes: [
+            "boulderId", "boulderDesc", "boulderLink",
+            "boulderReleaseDate", "boulderEndDate",
+            "difficultyId", "userId", "areaId", "boulderImageUrl"
+          ],
+          include: [
+            {
+              model: tbAreaGyms,
+              as: "area",
+              attributes: ["areaId", "areaName"]
+            }
+          ]
+        },
+        // Filtre root comments
+        {
+          model: tbReplies,
+          as: "isReplyOf",
+          required: false,
+          attributes: []
+        },
+        // Compte les replies
+        {
+          model: tbReplies,
+          as: "replies",
+          required: false,
+          attributes: []
+        }
+      ],
+      group: [
+        "tbComments.commentsId",
+        "author.userId",
+        "boulder.boulderId",
+        "boulder->area.areaId"
+      ],
+      order: [["commentsId", "DESC"]]
         })
         res.status(200).json(json);
     } catch (error) {
         res.status(500).json({ error: (error as any).message })
     }
 };
+export const getCommentsbyBoulders = async (req: Request, res: Response) => {
+  try {
+    const id = req.params.id as string;
+
+    const json = await tbComments.findAll({
+      where: {
+        [Op.and]: [
+          { boulderId: id },
+          Sequelize.where(
+            Sequelize.col("isReplyOf.commentsrepliesId"),
+            null
+          )
+        ]
+      },
+      attributes: {
+        include: [
+          [
+            Sequelize.fn("COUNT", Sequelize.col("replies.commentsId")),
+            "replyCount"
+          ]
+        ]
+      },
+      include: [
+        {
+          model: tbUsers,
+          as: "author",
+          attributes: ["userId", "userFName", "userLName", "userPseudo"]
+        },
+        {
+          model: tbBoulders,
+          as: "boulder",
+          attributes: [
+            "boulderId", "boulderDesc", "boulderLink",
+            "boulderReleaseDate", "boulderEndDate",
+            "difficultyId", "userId", "areaId", "boulderImageUrl"
+          ],
+          include: [
+            {
+              model: tbAreaGyms,
+              as: "area",
+              attributes: ["areaId", "areaName"]
+            }
+          ]
+        },
+        // Filtre root comments
+        {
+          model: tbReplies,
+          as: "isReplyOf",
+          required: false,
+          attributes: []
+        },
+        // Compte les replies
+        {
+          model: tbReplies,
+          as: "replies",
+          required: false,
+          attributes: []
+        }
+      ],
+      group: [
+        "tbComments.commentsId",
+        "author.userId",
+        "boulder.boulderId",
+        "boulder->area.areaId"
+      ],
+      order: [["commentsId", "DESC"]]
+    });
+
+    res.status(200).json(json);
+
+  } catch (error) {
+    res.status(500).json({ error: (error as any).message });
+  }
+};
+
 
 export const postComments = async (req:Request,res:Response) => {
     postElement(req,res,tbComments)
