0e7dfca94040515318f012e5a3c0c99ddfa6257e
EkssTV <matthieu.decreme@gmail.com>
2026-05-02 12:26:52 +0200

    fix(test): Oui les test bug mdr

--- server/src/models/tbAreaGyms.ts
+++ server/src/models/tbAreaGyms.ts
@@ -1,7 +1,7 @@
-import { DataTypes, Model } from "sequelize";
+import { DataTypes, Model} from "sequelize";
 import sequelize from "../config/database.js";
 
-class tbAreaGyms extends Model {
+class tbAreaGyms extends Model{
     declare areaId : number;
     declare areaName:string;
     declare areaDesc: string;
